efe921cded89043c86f5a9317bbb88758a045950
Kiripiro <alexandre.tourret77@gmail.com>
2022-09-20 15:48:27 +0200

    2FA frontend working

--- frontend/src/Page/2fa/2fa.tsx
+++ frontend/src/Page/2fa/2fa.tsx
@@ -6,15 +6,24 @@ const QrCode = (props: any) => {
     const [QrCode, setQrCode] = useState("");
     const [cookies, setCookie, removeCookie] = useCookies(["auth-cookie"]);
     const [code, setCode] = useState("");
+    const [res, setRes] = useState(0);
+    const [status, setStatus] = useState("Please enter the code above.");
 
     async function turnOn2fa(key: string) {
-        if (key == "Enter") axios.get('http://localhost:5001/auth/2fa/turn-on/' + code, { withCredentials: true }).then(res => console.log(res));
+        if (key == "Enter")
+            axios.get('http://localhost:5001/auth/2fa/turn-on/' + code, { withCredentials: true }).then(res => setRes(res.status)).catch((e) => setRes(e.response.status));
     }
 
     useEffect(() => {
-        console.log(QrCode)
+        console.log(res);
         if (QrCode === "")
-            axios.get('http://localhost:5001/auth/2fa/generate/' + cookies['auth-cookie'].refreshToken).then(res => (setQrCode(res.data)))
+             axios.get('http://localhost:5001/auth/2fa/generate/' + cookies['auth-cookie'].refreshToken).then(res => (setQrCode(res.data)))
+        if (res === 401)
+            setStatus("Error, wrong code.")
+        else if (res == 404)
+            setStatus("You must enter the code.")
+        else if (res == 200)
+            setStatus("2FA activated !")
     })
 
     return  <div className='2fa'>
@@ -26,6 +35,7 @@ const QrCode = (props: any) => {
                     onChange={(e) => setCode(e.target.value)}
                     onKeyDown={(e) => {turnOn2fa(e.key)}}
                 />
+                <p>{status}</p>
             </div>;
 }
 export default QrCode;
--- frontend/src/Page/Login/Callback.tsx
+++ frontend/src/Page/Login/Callback.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
@@ -17,30 +17,49 @@ export default function Callback() {
     //A MODIFIER ALED CA MARCHE PAS COMME ON VEUT
     const [cookies, setCookie, removeCookie] = useCookies(["auth-cookie"]);
     const [code, setCode] = useState("");
-    const [good, setgood] = useState(false);
-    console.log(cookies);
+    const [turnOn, setTurnOn] = useState(false);
+    const [res, setRes] = useState(0);
+    const [status, setStatus] = useState("");
+    //console.log(cookies);
 
     async function turnOn2fa(key: string) {
-        if (key == "Enter") axios.get('http://localhost:5001/auth/2fa/verify/' + code, { withCredentials: true }).then((e) => setgood(true));
+        if (key == "Enter") 
+            axios.get('http://localhost:5001/auth/2fa/verify/' + code, { withCredentials: true })
+                .then((e) => setTurnOn(true))
+                .catch((e) => {
+                    setRes(e.response.status)
+                });
     }
 
-    if (persistantReduceur.userReducer.user === null)
-        axios.get("http://localhost:5001/user/userExist/" + cookies["auth-cookie"].refreshToken).then((item) => { console.log('item.data', item.data); setUser(item.data) })
+    useEffect(() => {
+        if (res === 401)
+            setStatus("Error, wrong code.")
+        else if (res == 404)
+            setStatus("You must enter the code.")
+    })
+    
 
+    if (persistantReduceur.userReducer.user === null)
+        axios.get("http://localhost:5001/user/userExist/" + cookies["auth-cookie"].refreshToken).then((item) => { setUser(item.data); })
 
     if (persistantReduceur.userReducer.user !== null) {
 
-        if (persistantReduceur.userReducer.user.is2faEnabled && !good)
+        if (persistantReduceur.userReducer.user.is2faEnabled && !turnOn)
 
-            return (<input
-                type="text"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-                onKeyDown={(e) => { turnOn2fa(e.key) }}
-            />)
+            return (
+                <div className="login-2fa">
+                    <p>Enter your Google Authenticator code:</p>
+                    <input
+                        type="text"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                        onKeyDown={(e) => { turnOn2fa(e.key) }}
+                    />
+                    <p>{status}</p>
+                </div>
+            )
 
         else
-
             return (<Navigate to="/HomePage" />)
     }
     return (
