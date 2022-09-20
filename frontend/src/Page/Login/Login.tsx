312b9bbd22c612adffbed609fa0db55cfe088980
Wazack <64anex@gmail.com>
2022-09-20 14:49:19 +0200

    Login Page done

--- frontend/src/Page/Login/Login.tsx
+++ frontend/src/Page/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './../assets/Font.css';
-import './Login.css'
+import './Login.css';
+import logo from '../assets/logo_transcendence.png';
 
 function Login(props: { user?: any }) {
 	const [login, setLogin] = useState('')
@@ -16,13 +17,15 @@ function Login(props: { user?: any }) {
 	return (
 		<div className='Font'>
 			<div className='container'>
-				<button className="loginButton" onClick={onClick42Login}>
-					<img className="i42-logo" src="https://42lyon.fr/wp-content/uploads/2022/04/Artboard-1.svg" alt="" />
-				</button>
-				<input className="loginBar" value={login} onChange={(e) => { setLogin(e.target.value) }} onKeyDown={(event) => { if (event.key === 'Enter') onClickLoginSans42() }} placeholder='Enter valid login to connect' />
-				<button className="loginButton" onClick={onClickLoginSans42}>
-					<img className="i42-logo" src="https://42lyon.fr/wp-content/uploads/2022/04/Artboard-1.svg" alt="" />
-				</button>
+				<img className="logo_transcendence" src={logo} alt="" />
+				<button className="loginButton" onClick={onClick42Login}><span>SIGN IN WITH</span> <img src='https://stud.42lyon.fr/static/assets/images/42_logo_black.svg' className='logo42' /> <span>ACCOUNT</span></button>
+				
+				{/* TO REMOVE */}
+				<div className='dev'>
+					<input className="loginBar" value={login} onChange={(e) => { setLogin(e.target.value) }} onKeyDown={(event) => { if (event.key === 'Enter') onClickLoginSans42() }} placeholder='Enter valid login to connect' />
+					<button className="loginButton" onClick={onClickLoginSans42}><span>SIGN IN WITHOUT</span> <img src='https://stud.42lyon.fr/static/assets/images/42_logo_black.svg' className='logo42' /> <span>ACCOUNT</span></button>
+				</div>
+
 			</div>
 		</div>
 	)
