804acdce4d3654e5c7106b4fb59c613329757fd7
cgangaro <rocket4duck@outlook.com>
2022-09-26 12:30:28 +0200

    wsh

--- backend/src/api/messages/dtos/messages.dto.ts
+++ backend/src/api/messages/dtos/messages.dto.ts
@@ -5,12 +5,12 @@ export class MessagesDto {
 	@IsNotEmpty()
 	public id_sender: number;
 
-	@IsString()
-	public login_sender: string;
-
 	@IsNumber()
 	@IsNotEmpty()
 	public id_receiver: number;
+
+	@IsString()
+	public login_sender: string;
 
 	@IsString()
 	public login_receiver: string;
@@ -18,30 +18,3 @@ export class MessagesDto {
 	@IsString()
 	public text: string;
 }
-
-// import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
-// export class MessagesDto {
-// 	@IsNumber()
-// 	@IsNotEmpty()
-// 	public id_user1: number;
-
-// 	@IsNumber()
-// 	@IsNotEmpty()
-// 	public score_u1: number;
-
-// 	@IsNumber()
-// 	@IsNotEmpty()
-// 	public id_user2: number;
-
-// 	@IsNumber()
-// 	@IsNotEmpty()
-// 	public score_u2: number;
-
-// 	@IsNumber()
-// 	@IsNotEmpty()
-// 	public winner_id: number;
-
-// 	@IsString()
-// 	public text: string;
-// }
