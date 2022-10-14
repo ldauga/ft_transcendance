4b5d1565c259a88d9a2441fb5e867c1c25946eb1
cgangaro <rocekt4duck@hotmail.fr>
2022-10-14 11:30:30 +0200

    add: carousel for select map

--- frontend/src/Page/Pong/PongHome.tsx
+++ frontend/src/Page/Pong/PongHome.tsx
@@ -10,61 +10,56 @@ import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import NavBar from '../../Module/Navbar/Navbar';
 import './PongHome.scss';
 import map1 from './../assets/map1.png'
-import map2 from './../assets/map1.png'
+import map2 from './../assets/map2.png'
 import { useSelector } from 'react-redux';
 import { RootState } from '../../State';
 
-
-const images = [
+const steps = [
 	{
-		label: 'map1',
-		imgPath:
-			{ map1 },
+	  label: 'map1',
+	  description: map1,
 	},
 	{
-		label: 'map2',
-		imgPath:
-			{ map2 },
+	  label: 'map2',
+	  description: map2,
 	},
 	{
-		label: 'map3',
-		imgPath:
-			{ map1 },
+	  label: 'map3',
+	  description: map1,
 	},
 	{
-		label: 'map4',
-		imgPath:
-			{ map2 },
+	  label: 'map4',
+	  description: map2,
 	},
 ];
 
-const styles = {
-	slide: {
-		padding: 15,
-		minHeight: 100,
-		color: '#fff',
-	},
-	slide1: {
-		backgroundColor: '#FEA900',
-	},
-	slide2: {
-		backgroundColor: '#B3DC4A',
-	},
-	slide3: {
-		backgroundColor: '#6AC0FF',
-	},
-};
+// const styles = {
+// 	slide: {
+// 		padding: 15,
+// 		minHeight: 100,
+// 		color: '#fff',
+// 	},
+// 	slide1: {
+// 		backgroundColor: '#FEA900',
+// 	},
+// 	slide2: {
+// 		backgroundColor: '#B3DC4A',
+// 	},
+// 	slide3: {
+// 		backgroundColor: '#6AC0FF',
+// 	},
+// };
 
 function PongHome(props: any) {
+	const theme = useTheme();
+	const [activeStep, setActiveStep] = React.useState(0);
+	const maxSteps = steps.length;
+
 	const utilsData = useSelector((state: RootState) => state.utils);
 	const persistantReducer = useSelector((state: RootState) => state.persistantReducer);
 
 	const [inQueue, setInQueue] = React.useState(false);
 	const [notFound, setNotFound] = React.useState(false);
-
-	const theme = useTheme();
-	const [activeStep, setActiveStep] = React.useState(0);
-	const maxSteps = images.length;
 
 	function joinQueue() {
 		utilsData.socket.emit('JOIN_QUEUE', { user: persistantReducer.userReducer.user, gameMap: 'map1' });
@@ -99,53 +94,66 @@ function PongHome(props: any) {
 		<>
 			<NavBar />
 			<div className='pong'>
-				<Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-					<Paper
-						square
-						elevation={0}
+			<Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+				<Paper
+				square
+				elevation={0}
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+					height: 50,
+					pl: 2,
+					bgcolor: 'background.default',
+				}}
+				>
+				<Typography>{steps[activeStep].label}</Typography>
+				</Paper>
+				<Box
+						component="img"
 						sx={{
-							display: 'flex',
-							alignItems: 'center',
-							height: 50,
-							pl: 2,
-							bgcolor: 'background.default',
+						height: 255,
+						display: 'block',
+						maxWidth: 400,
+						overflow: 'hidden',
+						width: '100%',
 						}}
-					>
-						<Typography>{images[activeStep].label}</Typography>
-					</Paper>
-					<MobileStepper
-						steps={maxSteps}
-						position="static"
-						activeStep={activeStep}
-						nextButton={
-							<Button
-								size="small"
-								onClick={handleNext}
-								disabled={activeStep === maxSteps - 1}
-							>
-								Next
-								{theme.direction === 'rtl' ? (
-									<KeyboardArrowLeft />
-								) : (
-									<KeyboardArrowRight />
-								)}
-							</Button>
-						}
-						backButton={
-							<Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-								{theme.direction === 'rtl' ? (
-									<KeyboardArrowRight />
-								) : (
-									<KeyboardArrowLeft />
-								)}
-								Back
-							</Button>
-						}
+						src={steps[activeStep].description}
+						alt={steps[activeStep].label}
 					/>
-				</Box>
+				<MobileStepper
+				variant="dots"
+				steps={maxSteps}
+				position="static"
+				activeStep={activeStep}
+				nextButton={
+					<Button
+					size="small"
+					onClick={handleNext}
+					disabled={activeStep === maxSteps - 1}
+					>
+					Next
+					{theme.direction === 'rtl' ? (
+						<KeyboardArrowLeft />
+					) : (
+						<KeyboardArrowRight />
+					)}
+					</Button>
+				}
+				backButton={
+					<Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+					{theme.direction === 'rtl' ? (
+						<KeyboardArrowRight />
+					) : (
+						<KeyboardArrowLeft />
+					)}
+					Back
+					</Button>
+				}
+				/>
+		</Box>
 				<button className='join-queue' type='button' onClick={joinQueue}>{!inQueue ? 'Join queue' : 'Loading...'}</button>
 				<span>or</span>
-				<button onClick={() => props.setCreateMap(true)}>Create map</button>
+				<button className='create-map' onClick={() => props.setCreateMap(true)}>Create map</button>
 			</div>
 		</>
 	);
