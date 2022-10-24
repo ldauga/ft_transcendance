aa6d73747d044908249f7f559340df6acbb8c9c2
cgangaro <rocekt4duck@hotmail.fr>
2022-10-24 15:55:51 +0200

    add: all maps in carousel

--- frontend/src/Page/Pong/MapCarousel/MapCarousel.tsx
+++ frontend/src/Page/Pong/MapCarousel/MapCarousel.tsx
@@ -3,36 +3,41 @@ import './MapCarousel.scss'
 const steps = [
     {
         label: 'basic',
-        middle: 'none',
         ping: 'basic-ping-racket',
         pong: 'basic-pong-racket',
         ball: 'basic-ball',
     },
     {
         label: 'ia',
-        firstMiddle: 'ia-racket',
+        middle: 'ia-racket',
         ping: 'ia-ping-racket',
         pong: 'ia-pong-racket',
         ball: 'ia-ball',
     },
     {
-        label: 'ia',
-        firstMiddle: 'ia-racket',
-        secondMiddle: 'ia-racket',
-        ping: 'ia-ping-racket',
-        pong: 'ia-pong-racket',
-        ball: 'ia-ball',
+        label: 'expand',
+        middle: 'expand-middle',
+        ping: 'expand-ping-racket',
+        pong: 'expand-pong-racket',
+        ball: 'expand-ball',
+    },
+    {
+        label: 'create',
+        field: 'blur-field',
+        ping: 'create-ping-racket',
+        pong: 'create-pong-racket',
+        ball: 'create-ball',
     }
 ]
 
 function MapCarousel(props: any) {
     return (
         <div className="map-carousel">
-            <div className="field">
+            {steps[props.activeStep].field ? <span>?</span> : ''}
+            <div className={`field ${steps[props.activeStep].field}`}>
                 <div className="net"></div>
                 <div className={steps[props.activeStep].ping}></div>
-                <div className={steps[props.activeStep].firstMiddle}></div>
-                <div className={steps[props.activeStep].secondMiddle}></div>
+                <div className={steps[props.activeStep].middle}></div>
                 <div className={steps[props.activeStep].pong}></div>
                 {/* <div className='test-racket'></div> */}
                 <div className={steps[props.activeStep].ball}></div>
