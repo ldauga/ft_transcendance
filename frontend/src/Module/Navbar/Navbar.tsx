89a61f85cb1a5083988616b03d425419d5971cc9
Tourret Alexandre <atourret@student.42lyon.fr>
2022-11-11 10:37:28 +0100

    navbar with working searchbar

--- frontend/src/Module/Navbar/Navbar.tsx
+++ frontend/src/Module/Navbar/Navbar.tsx
@@ -228,68 +228,30 @@ function NavBar(props: { openFriendConversFromProfile: boolean, dataFriendConver
 					</Badge>
 				</button>
 				<button onClick={() => { //chat
-						setOpenPopUp(!open);
-						setAllNotifSeen()
-						if (isChat) {
-							setOpenPopUp(false);
-							setChat(false);
-						}
-						else if (isFriendList) {
-							setFriendList(false);
-							setChat(true);
-						}
-						else if (isNotif) {
-							setNotif(false);
-							setChat(true);
-						}
-						else
-							setChat(true);
-					}}>
-						<Badge color="error" badgeContent={(persistantReducer.notifReducer.notifArray.length - persistantReducer.notifReducer.notifArray.map(notif => notif.seen).filter(Boolean).length)}>
-							<Tooltip title="Notifications">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-									<path fillRule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clipRule="evenodd" />
-								</svg>
-							</Tooltip>
-						</Badge>
-					</button>
-					<button onClick={() => { //chat
-						setOpenPopUp(!open);
-						if (isChat) {
-							setOpenPopUp(false);
-							setChat(false);
-						}
-						else if (isFriendList) {
-							setFriendList(false);
-							setChat(true);
-						}
-						else if (isNotif) {
-							setNotif(false);
-							setChat(true);
-						}
-						else
-							setChat(true);
-					}}>
-						<Badge color="error" badgeContent={(persistantReducer.chatNotifReducer.total)}>
-							<Tooltip title="Chat">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-									<path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
-									<path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
-								</svg>
-							</Tooltip>
-						</Badge>
-					</button>
-					<Tooltip title="Account">
-						<div className='profile' onClick={handleClick}>
-							<img className='avatar' src={avatar} alt="avatar" />
-							<span>{nickname}</span>
-						</div>
-					<Tooltip title="Chat">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-						<path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
-						<path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
-					</svg>
-					</Tooltip>
+					setOpenPopUp(!open);
+					if (isChat) {
+						setOpenPopUp(false);
+						setChat(false);
+					}
+					else if (isFriendList) {
+						setFriendList(false);
+						setChat(true);
+					}
+					else if (isNotif) {
+						setNotif(false);
+						setChat(true);
+					}
+					else
+						setChat(true);
+				}}>
+					<Badge color="error" badgeContent={(persistantReducer.chatNotifReducer.total)}>
+						<Tooltip title="Chat">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+								<path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
+								<path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
+							</svg>
+						</Tooltip>
+					</Badge>
 				</button>
 				<Tooltip title="Account">
 					<div className='profile' onClick={handleClick}>
